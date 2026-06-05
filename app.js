cdda7209c4067e47d45f7d68c71017190aa7973a
Portal de projetos de desenvolvimento de equipes <invistanasuacarreirahoje@gmail.com>
2026-06-05 11:09:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const BIN_ID =
 '6a0c19da6877513b27975609';
 
 const API_KEY =
-'SUA_MASTER_KEY';
+'$2a$10$FHeRXKCTxHAD8HgExcIosujSiuAfP8pxLCkGF1wVKmD4n0t32vqWu';
 
 const API_URL =
 `https://api.jsonbin.io/v3/b/${BIN_ID}`;
